205272213395da69968d708106cf19bdab67fc65
Aaron Curtis <foobarbecue@gmail.com>
2015-06-03 19:21:34 +0000

    This is now a meteor package as well as a bower package.

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'foobarbecue:navgraph',
+  version: '0.0.1',
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/foobarbecue/navgraph',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('navgraph.js');
+  api.use('d3js:d3');
+  api.export('Navgraph','client');
+});
